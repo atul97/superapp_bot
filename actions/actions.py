45320603ad7b28c92d9a3fabb59bcb405ec4aa94
atul97 <atul.aggrawal97@gmail.com>
2023-01-09 12:03:08 +0530

    add fallback,customer membership

--- actions/actions.py
+++ actions/actions.py
@@ -1,32 +1,9 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import AllSlotsReset, SlotSet
 
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        # authentic_user = tracker.get_slot("authentic_user") 
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
 
 import requests
 import psycopg2
@@ -41,7 +18,45 @@ class ActionSendOTP(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        authentic_user = tracker.get_slot("authentic_user") 
+        authentic_user = tracker.get_slot("authentic_user")
+        otp = tracker.get_slot("otp")
+
+
+        conn = None
+        try:
+            mobile = tracker.get_slot("mobile")
+            print(mobile)
+            conn = psycopg2.connect(database ="sapp", user = "postgres",
+                        password = "123456", host = "localhost", 
+                        port = "5432")
+            print("Connection Successful to PostgreSQL")
+
+            cur = conn.cursor()
+            
+            query = f"""select * from customer where phone_number = '{mobile}';"""
+            print(query)
+            cur.execute(query)
+            rows = cur.fetchall()
+
+
+            
+            cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+                print('Database connection closed.')
+        if len(rows)>0:
+            # dispatcher.utter_message(text=f"No details found for user with contact: {mobile} ")
+            print("User Exist")
+            authentic_user = "True"
+            otp = "1234"
+
+
+
+
         if authentic_user !="True":
             try:
                 
@@ -67,7 +82,7 @@ class ActionSendOTP(Action):
                 dispatcher.utter_message(text="Something went wrong")
             
 
-        return []
+        return [SlotSet('authentic_user',authentic_user),SlotSet('otp',otp)]
 
 class ActionVerifyOTP(Action):
 
@@ -80,6 +95,7 @@ class ActionVerifyOTP(Action):
 
 
         authentic_user = tracker.get_slot("authentic_user") 
+        print(f"Auth_user:{authentic_user}")
         if authentic_user !="True":
             try:
                 otp = tracker.get_slot("otp")
@@ -134,11 +150,11 @@ class ActionCheckCustomer(Action):
                 conn = psycopg2.connect(database ="sapp", user = "postgres",
                             password = "123456", host = "localhost", 
                             port = "5432")
-                print("Connection Successful to PostgreSQL")
+                print("Connection Successful to PostgreSQL ActionCheckCustomer")
 
                 cur = conn.cursor()
                 
-                query = f"""select * from customer where phone_number = '{mobile}';"""
+                query = f"""select * from customer where phone_number = '{mobile}' and flag_exist = 1;"""
                 cur.execute(query)
                 rows = cur.fetchall()
 
@@ -153,12 +169,45 @@ class ActionCheckCustomer(Action):
                     conn.close()
                     print('Database connection closed.')
             if len(rows)==0:
-                dispatcher.utter_message(text=f"No details found for user with contact: {mobile} ")
+                # dispatcher.utter_message(text=f"No details found for user with contact: {mobile} ")
 
-                buttons = []
-                buttons.append({"title": "Add new customer" , "payload": "add_customer"})
-                buttons.append({"title": "Explore More" , "payload": "explore_more"})
-                dispatcher.utter_message(text="Want to add customer",buttons=buttons)
+                try:
+                    row2 = 0
+                    print("-----------")
+                    mobile = tracker.get_slot("mobile")
+                    conn = psycopg2.connect(database ="sapp", user = "postgres",
+                                password = "123456", host = "localhost", 
+                                port = "5432")
+                    print("Connection Successful to PostgreSQL if user not existed")
+
+                    cur = conn.cursor()
+                    
+                    query = f"""select * from customer where phone_number = '{mobile}';"""
+                    cur.execute(query)
+                    row2 = cur.fetchall()
+                    
+                    cur.close()
+                except (Exception, psycopg2.DatabaseError) as error:
+                    
+                    print(error)
+                finally:
+                    if conn is not None:
+                        conn.close()
+                        print('Database connection closed.')
+
+                if len(row2)==0:
+                    dispatcher.utter_message(text=f"No details found for user with contact: {mobile}")
+
+                    buttons = []
+                    buttons.append({"title": "Add new customer" , "payload": "add_customer"})
+                    buttons.append({"title": "Explore More" , "payload": "explore_more"})
+                    dispatcher.utter_message(text="Want to add customer",buttons=buttons)
+
+                else:
+                    buttons = []
+                    buttons.append({"title": "Activate customer" , "payload": "activate_customer"})
+                    buttons.append({"title": "Explore More" , "payload": "explore_more"})
+                    dispatcher.utter_message(text="Want to activate customer",buttons=buttons)
             else:
                 dispatcher.utter_message(text="User existed")
                 dispatcher.utter_message(template=f"utter_main_menu")
@@ -166,6 +215,92 @@ class ActionCheckCustomer(Action):
             dispatcher.utter_message(text=f"Please authenticate")
 
         return []
+
+
+
+class ActionActivateCustomer(Action):
+
+    def name(self) -> Text:
+        return "action_activate_customer"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        authentic_user = tracker.get_slot("authentic_user") 
+
+        if authentic_user =="True":
+            conn = None
+            try:
+                print("-----------")
+                mobile = tracker.get_slot("mobile")
+
+                conn = psycopg2.connect(database ="sapp", user = "postgres",
+                            password = "123456", host = "localhost", 
+                            port = "5432")
+                print("Connection Successful to PostgreSQL")
+
+                cur = conn.cursor()
+                
+                query = f"""update customer set flag_exist = 1 where phone_number = '{mobile}';"""
+                cur.execute(query)
+                conn.commit()
+
+                
+                cur.close()
+            except (Exception, psycopg2.DatabaseError) as error:
+                
+                print(error)
+            finally:
+                if conn is not None:
+                    conn.close()
+                    print('Database connection closed.')
+        else:
+            dispatcher.utter_message(text=f"Please authenticate")
+        return [SlotSet('order_type',None)]
+
+
+class ActionDeactivateCustomer(Action):
+
+    def name(self) -> Text:
+        return "action_deactivate_customer"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        authentic_user = tracker.get_slot("authentic_user") 
+
+        if authentic_user =="True":
+            conn = None
+            try:
+                print("-----------")
+                mobile = tracker.get_slot("mobile")
+
+                conn = psycopg2.connect(database ="sapp", user = "postgres",
+                            password = "123456", host = "localhost", 
+                            port = "5432")
+                print("Connection Successful to PostgreSQL")
+
+                cur = conn.cursor()
+                
+                query = f"""update customer set flag_exist = 0 where phone_number = '{mobile}';"""
+                cur.execute(query)
+                conn.commit()
+
+                
+                cur.close()
+            except (Exception, psycopg2.DatabaseError) as error:
+                
+                print(error)
+            finally:
+                if conn is not None:
+                    conn.close()
+                    print('Database connection closed.')
+        else:
+            dispatcher.utter_message(text=f"Please authenticate")
+        return [SlotSet('order_type',None)]
+
 
 class ActionExistingOrder(Action):
 
@@ -436,3 +571,106 @@ class ActionResourcesList(Action):
 
         dispatcher.utter_message(attachment=test_carousel)
         return []
+
+
+
+
+
+# class ActionWriteToDB(Action):
+
+#     def name(self) -> Text:
+#         return "action_Write_to_DB"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         authentic_user = "True"
+
+#         if authentic_user =="True":
+#             conn = None
+#             try:
+#                 print("-----------")
+
+#                 mobile = "9953635285"
+#                 order_type = "hyperlocal"
+#                 conn = psycopg2.connect(database ="sapp", user = "postgres",
+#                             password = "123456", host = "localhost", 
+#                             port = "5432")
+#                 print("Connection Successful to PostgreSQL")
+
+#                 cur = conn.cursor()
+                
+#                 query = f"""select * from orders where phone_number = '{mobile}' and order_type = '{order_type}';"""
+#                 cur.execute(query)
+#                 rows = cur.fetchall()
+#                 for i in rows :
+#                     dispatcher.utter_message(text=f"Data: {i[2]} , Status: {i[3]}, Amount: {i[4]}")
+
+                
+#                 cur.close()
+#             except (Exception, psycopg2.DatabaseError) as error:
+                
+#                 print(error)
+#             finally:
+#                 if conn is not None:
+#                     conn.close()
+#                     print('Database connection closed.')
+#             if len(rows)==0:
+#                 dispatcher.utter_message(text=f"No details found for user with contact: {mobile}  for {order_type} order")
+#         else:
+#             dispatcher.utter_message(text=f"Please authenticate")
+#         return [SlotSet('order_type',None)]
+
+
+
+
+
+
+
+
+# class ActionRemoveCustomer(Action):
+
+#     def name(self) -> Text:
+#         return "action_remove_customer"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         authentic_user = "True"
+
+#         if authentic_user =="True":
+#             conn = None
+#             try:
+#                 print("-----------")
+
+#                 mobile = "9953635285"
+#                 order_type = "hyperlocal"
+#                 conn = psycopg2.connect(database ="sapp", user = "postgres",
+#                             password = "123456", host = "localhost", 
+#                             port = "5432")
+#                 print("Connection Successful to PostgreSQL")
+
+#                 cur = conn.cursor()
+                
+#                 query = f"""select * from orders where phone_number = '{mobile}' and order_type = '{order_type}';"""
+#                 cur.execute(query)
+#                 rows = cur.fetchall()
+#                 for i in rows :
+#                     dispatcher.utter_message(text=f"Data: {i[2]} , Status: {i[3]}, Amount: {i[4]}")
+
+                
+#                 cur.close()
+#             except (Exception, psycopg2.DatabaseError) as error:
+                
+#                 print(error)
+#             finally:
+#                 if conn is not None:
+#                     conn.close()
+#                     print('Database connection closed.')
+#             if len(rows)==0:
+#                 dispatcher.utter_message(text=f"No details found for user with contact: {mobile}  for {order_type} order")
+#         else:
+#             dispatcher.utter_message(text=f"Please authenticate")
+#         return [SlotSet('order_type',None)]
